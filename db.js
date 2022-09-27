bf1f6ff691bba9885eab8cf63d3150c04454af60
MEHEDIs PC <miraz.onesoft@gmail.com>
2022-09-27 21:27:09 +0600

    initial

---
+++ db.js
@@ -0,0 +1,9 @@
+const users = require("./data/users.json");
+const teams = require("./data/teams.json");
+const projects = require("./data/projects.json");
+
+module.exports = {
+    users,
+    teams,
+    projects,
+};
